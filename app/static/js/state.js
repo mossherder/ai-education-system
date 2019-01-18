968db6e9ed8745bb054b8baf30c1a138770ef935
Mark <mark.mendez@faithtechnologies.com>
2019-01-18 15:46:38 -0600

    Created initial state controller abstraction

--- app/static/js/state.js
+++ app/static/js/state.js
@@ -1,26 +1,47 @@
 const appStateCore = {
-  intitalize: function() {
+  initialize: function() {
     appStateUI.initialize();
   },
 
   stateController: {
     graphicsController: {
-      setGraphics: function(a, b, c) {
-        const graphicsArray = [a, b, c];
-        appStateUI.updateGraphics(graphicsArray);
+      showGraphics: function() {
+        appStateUI.showGraphics();
       }
     },
     equationController: {
-      setEquation: function(a, b, c) {
-        const equationArray = [a, b, c];
-        appStateUI.updateEquation(equationArray);
+        setEquation: function(a,b,c) {
+          let operator = null;
+          if (c === 0) {
+            operator = operatorCore.add;
+          }
+          if (c === 1) {
+            operator = operatorCore.subtract;
+          }
+          const result = operator(Number(a), Number(b))
+          const operatorSymbol = result.symbol;
+          const solution = result.solution;
+          const displayArray = [a, operatorSymbol, b, solution]
+          appStateUI.updateEquation(displayArray);
+        },
+        showSolution: function() {
+          appStateUI.showSolution();
+        }
       }
     },
     textController: {
-      setText: function(a, b, c) {
-        const textArray = [a, b, c];
-        appStateUI.updateText(textArray);
+      showText: function() {
+        appStateUI.showText();
+      }
+    },
+    pointsFlow: {
+      updatePoints: function() {
+        const points = gatherCurrentPoints();
+        tfSystemCore.pointPassBack(points);
+      },
+      gatherCurrentPoints: function() {
+        // Does the system decide points or the algorithm??
+        return 0;
       }
     }
   }
-}
