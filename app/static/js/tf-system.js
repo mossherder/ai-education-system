1db1e5ba6af6d5e01d1dc68feed1b45a0cda8d7a
Mark <mark.mendez@faithtechnologies.com>
2019-01-18 15:47:41 -0600

    create initial system controls

--- app/static/js/tf-system.js
+++ app/static/js/tf-system.js
@@ -1,6 +1,27 @@
 const tfSystemCore = {
-  initialize: function() {
+  controller: appStateCore.stateController,
 
+  initialize: function() {
+    return null;
   },
-  
+
+  createEquation: function(a, b, operator) {
+    tfSystemCore.controller.equationController.setEquation(a, b, operator);
+  },
+
+  showSolution: function() {
+    tfSystemCore.controller.equationController.showSolution();
+  },
+
+  showGraphics: function() {
+    tfSystemCore.controller.graphicsController.showGraphics();
+  },
+
+  showText: function() {
+    tfSystemCore.controller.textController.showText();
+  },
+
+  pointPassBack: function(points) {
+    // pass current points back to algorithm
+  }
 }
